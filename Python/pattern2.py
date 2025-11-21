e9fdbf2c31b0bd8369b93df81ac875cb2a1ab2b2
Vihan <vihanthrj64@gmail.com>
2025-11-21 21:13:34 +0530

    Add pattern printing functionality in pattern2.py
    Implement a pattern printing program that displays a decreasing sequence of numbers based on user input.

---
+++ Python/pattern2.py
@@ -0,0 +1,15 @@
+# Pattern
+# 5
+# 5 4
+# 5 4 3
+# 5 4 3 2
+# 5 4 3 2 1
+
+num = int(input("Enter number of rows "))
+
+# main stuff here but its a bit confusing to me
+for i in range (1,5):
+   for j in range (5,5-num,-1):
+     print(j,end = " ")
+  print()
+          
