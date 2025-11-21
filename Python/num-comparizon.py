9e80ac56ea034fe6615f4667c1b6f985c2609d44
Vihan <vihanthrj64@gmail.com>
2025-11-21 19:28:21 +0530

    Add number comparison functionality

---
+++ Python/num-comparizon.py
@@ -0,0 +1,10 @@
+a = int(input("Enter 1st number "))
+b = int(input("Enter 2nd number "))
+
+# main stuff under here
+if( a > b):
+  print(a," is greater than ",b)
+elif(a < b):
+  print(b," is greater than ",a)
+else:
+  print("Both numbers are equal")
