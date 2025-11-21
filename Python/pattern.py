d46c26e442cfac3fb74d425f33d064f7f3756b1d
Vihan <vihanthrj64@gmail.com>
2025-11-21 19:01:05 +0530

    Implement pattern printing in pattern.py
    Add a pattern printing program that prints numbers in a triangular format based on user input.

---
+++ Python/pattern.py
@@ -0,0 +1,16 @@
+#Pattern-
+
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
+
+n=int(input("Enter number of lines - "))
+i=1
+
+for i in range n:#loop for number of rows
+   for j in range i:#loop for colomns
+      print(j,"   ")
+     
+      
