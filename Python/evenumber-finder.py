dba8f00fe556ff2fd1e40ed1a04d32ef08344754
Vihan <vihanthrj64@gmail.com>
2025-11-21 19:16:56 +0530

    Implement even number checker in evenumber-finder.py
    Add a program to check if a number is even or odd.

---
+++ Python/evenumber-finder.py
@@ -0,0 +1,9 @@
+#Write a program to check if a number if even or not
+num=int(input("Enter a number- "))
+
+#main stuff under here
+if(a%2==0):
+  print("The number ",num," is even")
+
+else:
+  print("The number ",num," is odd")
