2bba5b37bd1a183fc6b5aa28551cc8e5314b8ea8
Vihan <vihanthrj64@gmail.com>
2025-11-23 13:52:16 +0530

    Add name and age printing functionality

---
+++ Python/nameAgePrint.py
@@ -0,0 +1,9 @@
+# Print the name and age of the user input
+
+name = input("Enter your name ")
+age = int(input("Enter your age "))
+
+print("Your name is ",name)
+print("Your age is ",age)
+
+# yes i know "smartest" program ever
